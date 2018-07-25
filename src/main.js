9f823a833297d73255c54b11fa357368764c9f11
hufg <hufg@qq.com>
2018-07-25 21:40:20 +0800

    完成了顶部与底部的静态布局&首页结构构架

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 引入路由
+import VueRouter from 'vue-router'
+//引入index组件
+import index from './components/index.vue'
+//路由中间件
+Vue.use(VueRouter)
+// 路由实例化   
+const router = new VueRouter({
+  // 定义路由
+  routes:[
+    { path: '/index', component: index }
+   
+  ]
+})
+// 公共样式
+import './assets/statics/site/css/style.css';
 Vue.config.productionTip = false
 
 new Vue({
+  //选择器
+ el: '#app',
+ //挂载路由
+ router,
   render: h => h(App)
-}).$mount('#app')
+})
